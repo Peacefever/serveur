68266eb6077dde7e3ee8a0f2339670f2ee50fea9
Peacefever <yann.donzet@imerir.com>
2017-06-26 02:49:35 +0200

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -31,7 +31,7 @@ def addPlayer():
     data = request.get_json()
     if 'name' in data:
         #table = "{\"name\": \""+data['name']+"\",\"infoPlayer\": {\"location\": [{\"latitude\": 25}, {\"longitude\": 50}],\"argent\": [{\"dispo\": 1.0}, {\"ventes\": 0.0}, {\"profit\": 0.0}]}}"
-        table = "{\"name\": \""+db.rowcount()+"\",\"infoPlayer\": {\"location\": [{\"latitude\": 25}, {\"longitude\": 50}],\"argent\": [{\"dispo\": 1.0}, {\"ventes\": 0.0}, {\"profit\": 0.0}]}}"
+        table = "{\"name\": \""+db.rowcount+"\",\"infoPlayer\": {\"location\": [{\"latitude\": 25}, {\"longitude\": 50}],\"argent\": [{\"dispo\": 1.0}, {\"ventes\": 0.0}, {\"profit\": 0.0}]}}"
     print table
     return json.dumps(table), 200, { "Content-Type": "application/json" }
 
