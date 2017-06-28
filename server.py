4f539c7101296ab4405bec103ec27642246347ae
Peacefever <yann.donzet@imerir.com>
2017-06-28 14:53:47 +0200

    Update server.py

--- server.py
+++ server.py
@@ -423,17 +423,7 @@ def collect_sales():
 			return to_make_response('Internal Server Error', 500)
 
 			#Update de la ligne en question
-		db.execute("UPDATE Sales SET (quantity_sales = %(quantity)s, day_sales =%(day)s, \
-			id_player = %(p_id)s, id_recipe = %(r_id)s WHERE day_sales = %(old)s ANd id_player = %(old_p)s AND\
-			id_recipe = %(old_r)s)",{
-			"old_r":soldToModify[0]['id_recipe'],
-			"quantity": dictObject['quantity'],
-			"day": currentDay,
-			"p_id": playerID,
-			"r_id": recipeID,
-			"old":soldToModify[0]['day_sales'],
-			"old_p":soldToModify[0]['id_player']
-			})
+		db.execute("UPDATE sales SET (quantity_sales = %(quantity)s, day_sales =%(day)s, id_player = %(p_id)s, id_recipe = %(r_id)s WHERE day_sales = %(old)s AND id_player = %(old_p)s AND id_recipe = %(old_r)s)",{"quantity": dictObject['quantity'], "day": currentDay, "p_id": playerID, "r_id": recipeID,"old":soldToModify[0]['day_sales'],"old_p":soldToModify[0]['id_player'],"old_r":soldToModify[0]['id_recipe']})
 
 		print(db.select("SELECT * FROM Sales"))
 	db.close()
