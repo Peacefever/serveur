11f44225188d47522e83052e550aa19f81ab8c78
Peacefever <yann.donzet@imerir.com>
2017-06-28 15:42:12 +0200

    Update server.py

--- server.py
+++ server.py
@@ -350,7 +350,7 @@ def metro_save_infos():
 def collect_sales():
 	'''
 	Cette route permet de sauvegarder le nombre de ventes
-	de chaque boissons pour un joeuur donné
+	de chaque boissons pour un jour donné
 	'''
 	datas = request.get_json()
 	#datas = {"sales":[{"player":"toto", "item":"Limonade", "quantity":2}]}
@@ -431,7 +431,7 @@ def collect_sales():
 		print "old":soldToModify[0]['day_sales']
 		print "old_p":soldToModify[0]['id_player']
 		print "old_r":soldToModify[0]['id_recipe']
-		'''
+		
 		db.execute("UPDATE sales SET quantity_sales = %(quantity)s, day_sales =%(day)s, id_player = %(p_id)s \
 		, id_recipe = %(r_id)s \
 		WHERE day_sales = %(old)s AND id_player = %(old_p)s AND id_recipe = %(old_r)s)",{
@@ -443,7 +443,7 @@ def collect_sales():
 			"old_p":soldToModify[0]['id_player'],
 			"old_r":soldToModify[0]['id_recipe']
 		})
-
+		'''
 		print(db.select("SELECT * FROM Sales"))
 	db.close()
 	return to_make_response('', 201)
